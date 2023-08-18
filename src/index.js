a3f71194f1a4e9b7fa4c78d86700230cc14cd11b
delanovictor <delanovictor3@gmail.com>
2023-08-18 10:02:22 -0300

    removed: x-ray and pg

--- src/index.js
+++ src/index.js
@@ -1,20 +1,14 @@
 
-const AWSXRay = require('aws-xray-sdk');
-AWSXRay.captureHTTPsGlobal(require('http'));
-
-
-const { Client } = AWSXRay.capturePostgres(require('pg'))
 const express = require('express');
 const axios = require('axios');
 
 (async () => {
     const app = express()
 
-    app.use(AWSXRay.express.openSegment('simple-api'));
-
     app.use(express.json());
 
     const port = process.env.API_PORT || 80
+
     let i = 0
 
     app.listen(port, () => {
@@ -32,33 +26,6 @@ const axios = require('axios');
         res.send(response)
     })
 
-    app.post('/process', async (req, res) => {
-
-        const delay = req.body.delay
-        const response = { 'message': "Processado!", 'request_id': i, 'delay': delay }
-
-        console.log('Processando...')
-
-        AWSXRay.captureAsyncFunc('query-data', function (subsegment) {
-            setTimeout(() => {
-
-                subsegment.close();
-            }, 500)
-        });
-
-        AWSXRay.captureAsyncFunc('process-data', function (subsegment) {
-            setTimeout(() => {
-
-                console.log('Processado!')
-
-                subsegment.close();
-                res.send(response)
-
-            }, delay ? 1000 : 200)
-        });
-
-    })
-
     app.post('/time', async (req, res) => {
         const delay = req.body.delay
         const response = { 'message': "Request Executado!", 'request_id': i, 'delay': delay }
@@ -73,58 +40,6 @@ const axios = require('axios');
         } catch (e) {
             response.error = e
         }
-
-        AWSXRay.captureAsyncFunc('process-api-result', function (subsegment) {
-
-            setTimeout(() => {
-                console.log('Processado!')
-
-                subsegment.close();
-
-                res.send(response)
-
-            }, delay ? 1000 : 200)
-
-        });
-
     })
 
-    app.get('/connect', async (req, res) => {
-        try {
-            const client = new Client({
-                host: process.env.DB_HOST,
-                port: process.env.DB_PORT || 5432,
-                user: process.env.DB_USER,
-                password: process.env.DB_PASSWORD,
-            })
-            await client.connect()
-
-            const result = await client.query('SELECT version()')
-
-            // Retrieve the most recently created subsegment
-            const subs = AWSXRay.getSegment().subsegments;
-
-            if (subs && subs.length > 0) {
-                var sqlSub = subs[subs.length - 1];
-                sqlSub.sql.sanitized_query = queryString;
-            }
-
-            const version = result.rows[0].version
-
-            await client.end()
-
-            const response = { 'message': "Conectado ao banco", 'version': version, 'request_id': i }
-            console.log(response)
-            res.send(response)
-        } catch (e) {
-            const error = { 'message': 'Erro ao se conectar ao banco', 'request_id': i }
-            console.log(error)
-            console.log(e)
-
-            res.status(500);
-            res.send(error)
-        }
-    })
-
-    app.use(AWSXRay.express.closeSegment());
 })()
